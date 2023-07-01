f260fe8eca61789dd2f5d19ef39924e8596a0867
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-07-01 23:07:56 +0200

    Weird behavior of tests

--- src/main/kotlin/it/move2/agent/application/Updater.kt
+++ src/main/kotlin/it/move2/agent/application/Updater.kt
@@ -1,9 +1,20 @@
 package it.move2.agent.application
 
 import it.move2.agent.ports.JobBoard
+import it.move2.job.JobOffer
 
 class Updater(
     private val jobBoards: List<JobBoard>, private val publisher: Publisher
 ) {
-    fun execute() = jobBoards.map { it.getJobs() }.flatten().let { publisher.publish(it) }
+    //    fun execute() = jobBoards.map { it.getJobs() }.flatten().let { publisher.publish(it) }
+    fun execute() {
+        val jobList = mutableListOf<JobOffer>()
+        for (jobBoard in jobBoards) {
+            val jobs = jobBoard.getJobs()
+            for (job in jobs) {
+                jobList.add(job)
+            }
+        }
+        publisher.publish(jobList)
+    }
 }
